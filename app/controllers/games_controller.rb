eda693ff65f8a8ebc0c9edee48c591b2b42da2cc
Valeria Behrend <valu-sp@hotmail.com>
2021-07-06 23:34:48 +0200

    letters randome ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,10 @@
 class GamesController < ApplicationController
+    attr_accessor :letters
     def new
         @letters = (0...9).map { (65 + rand(26)).chr }
     end
 
     def score
+        @word = params[:word]
     end
 end
