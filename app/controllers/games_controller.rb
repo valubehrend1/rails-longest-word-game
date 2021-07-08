2f0f75414fc30f1f6bd101a3f905b30ac11681bc
Valeria Behrend <valu-sp@hotmail.com>
2021-07-08 20:57:15 +0200

    almost done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,5 +6,24 @@ class GamesController < ApplicationController
 
     def score
         @word = params[:word]
+        @letters = params[:letters]
+        @result = include_letters?(@word, @letters)
+        @display_result = result
+    end
+
+    def result
+        if @result == false
+           return  "Sorry but #{@word} can't be built out of #{@letters} "
+        else
+            return "You win!"
+        end
+    end
+
+    def include_letters?(word, letters)
+        grid_word = word.downcase.split('')
+        user_word = letters.downcase.split('')
+        grid_word.all? do |letter|
+           user_word.count(letter) >= grid_word.count(letter)
+        end
     end
 end
